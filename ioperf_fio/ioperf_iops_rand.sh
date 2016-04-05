e02fec76b439f83a9dad6c7e7ad4b832b1de7411
jzou <jzou@BJOops01.dev.fwmrm.net>
2016-04-05 05:51:50 +0000

    modify target file dir structure

--- ioperf_fio/ioperf_iops_rand.sh
+++ ioperf_fio/ioperf_iops_rand.sh
@@ -1,38 +1,38 @@
 #! /bin/bash
-# From Matrix Zou for NFS/GlusterFS Benchmark (iops)
-# Maintainer	: jzou@freewheel.tv
-# Version	: "0.1-20160314"
+# From Matrix Zou for NFS/GlusterFS Benchmark (randwrite/randread)
+# Maintainer    : jzou@freewheel.tv
+# Version       : "0.1-20160314"
 #
 # $1 = IO performace Benchmark IO Pattern (randrw, randread, randwrite)
 # $2 = Filesytems/mount point will be tested with
 # $3 = Result Path
 #
 # crontab -u root -e
-# 21 * * * * bash  /<path>/ioperf_iops.sh randrw /mnt/gluster /tmp/gluster-randrw-bond
+# 21 * * * * bash  /<path>/ioperf_iops_rand.sh randwrite /mnt/gluster /tmp/gluster-randwrite
 #
 
 if [ "$3" == "" ] ; then
-	echo " "
-	echo "missing arguments:"
-	echo "------------------"
-	echo " arg1 = IO performace Benchmark IO Pattern 		e.g. randrw/randread/randwrite"
-	echo " arg2 = Filesytems/mount point will be tested with	e.g. /mnt/gluster"
-	echo " arg3 = Result Path					e.g. /tmp/gluster-randrw-bond (where to create result files)"
-	echo " NOTE: If result path is existed, all data in it will be removed."
-	echo "------------------"
-	echo " "
-	exit
+        echo " "
+        echo "missing arguments:"
+        echo "------------------"
+        echo " arg1 = IO performace Benchmark IO Pattern                e.g. randrw/randread/randwrite"
+        echo " arg2 = Filesytems/mount point will be tested with        e.g. /mnt/gluster"
+        echo " arg3 = Result Path                                       e.g. /tmp/gluster-randrw-bond (where to create result files)"
+        echo " NOTE: If result path is existed, all data in it will be removed."
+        echo "------------------"
+        echo " "
+        exit
 fi
 
-if [ ! -f iops.fio ] ; then
-	echo "Oops! iops.fio profile file is not exited!"
-	exit
+if [ ! -f randiops.fio ] ; then
+        echo "Oops! fio profile file (randiops.fio) is not exited!"
+        exit
 fi
 
-if [ ! -x /usr/bin/fio ]; then 
+if [ ! -x /usr/bin/fio ]; then
         echo "can not locate fio, not installed?"
-	echo "Installation Steps:"
-	echo "rpm -ivh http://pkgs.repoforge.org/fio/fio-2.1.10-1.el7.rf.x86_64.rpm"
+        echo "Installation Steps:"
+        echo "rpm -ivh http://pkgs.repoforge.org/fio/fio-2.1.10-1.el7.rf.x86_64.rpm"
         exit
 fi
 
@@ -45,21 +45,24 @@ rm -rf $base_dir
 mkdir -p $base_dir
 echo `hostname` > $base_dir/hostname
 
-FORMAT=/mnt/nfsv3_bond/`hostname -s`.\$jobnum.\$filenum
-echo $FORMAT
+hostname=`hostname -s`
+mkdir ${target}/${hostname} > /dev/null 2>&1
+for i in `seq 0 99`; do
+        if [ ! -d ${target}/${hostname}/$i ]; then
+                mkdir ${target}/${hostname}/$i
+        fi
+ done
 
 mkdir -p $base_dir/4K
 for threads in {32,64,100}; do
 {
-	mkdir -p $base_dir/4K/${threads}threads
-	i=0
-#	while [ $i -lt 10 ]; do 
-	echo ===================================Test Start $(date +\%H\%d\%m)=================================== >> $base_dir/4K/${threads}threads/${test_type}
-	TYPE=${test_type} THREADS=$threads FORMAT="${target}/`hostname -s`.\$jobnum.\$filenum" fio /root/randiops.fio >> $base_dir/4K/${threads}threads/${test_type}
-	echo ===================================Test Stop $(date +\%H\%d\%m)=================================== >> $base_dir/4K/${threads}threads/${test_type}
-	let i=$i+1;
-#	done
-	unset i
+        mkdir -p $base_dir/4K/${threads}threads
+        echo ===================================Test Start $(date +\%H\%d\%m)=================================== >> $base_dir/4K/${threads}threads/${test_type}
+        TYPE=${test_type} THREADS=$threads FORMAT="${target}/${hostname}/\$jobnum/\$filenum" fio /root/randiops.fio >> $base_dir/4K/${threads}threads/${test_type}
+        echo ===================================Test Stop $(date +\%H\%d\%m)=================================== >> $base_dir/4K/${threads}threads/${test_type}
 }
 done
+
+echo `hostname -s` done
+
 
