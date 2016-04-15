45c9c3ae1de45fcaa70714a6a83ffaceb427f68f
matrix.zj <matrix.zj@qq.com>
2016-04-15 11:25:24 +0000

    repo_sync.sh: remove unset

--- repo_sync.sh
+++ repo_sync.sh
@@ -16,7 +16,7 @@ yum --showduplicates list available --disablerepo="*" --enablerepo=${channel} | 
 sed -i ':a;/'${channel}'$/!{N;s/\n//;ba}' /tmp/${channel}.raw-list
 sed -i 's/[0-9]://' /tmp/${channel}.raw-list
 
-awk '{print$2}' /tmp/${channel}.raw-list | awk -F. 'BEGIN{OFS="."}{NF-=1;print}'| awk '{print NR,$0}' > /tmp/${channel}.name-list
+awk '{print$1}' /tmp/${channel}.raw-list | awk -F. 'BEGIN{OFS="."}{NF-=1;print}'| awk '{print NR,$0}' > /tmp/${channel}.name-list
 awk  '{print NR,$1}' /tmp/${channel}.raw-list | awk -F. '{print NR,$NF}' > /tmp/${channel}.arch-list
 awk  '{print NR,$2}' /tmp/${channel}.raw-list > /tmp/${channel}.version-list
 
@@ -27,6 +27,3 @@ for line in `cat /tmp/${channel}.rpmlist`; do yumdownloader --destdir=${dst_dir}
 
 rm -f /tmp/${channel}.* 
 sed -i '/^enabled/s/0/1/' /etc/yum/pluginconf.d/versionlock.conf
-
-unset ${channel}
-unset ${dst_dir}
