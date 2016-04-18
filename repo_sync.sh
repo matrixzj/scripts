aef7fe17e13e3d20b83cbcad838f463dd9c63ed8
matrix.zj <matrix.zj@qq.com>
2016-04-18 16:33:51 +0000

    repo_sync.sh: multithread added

--- repo_sync.sh
+++ repo_sync.sh
@@ -9,9 +9,11 @@
 
 channel=$1
 dst_dir=$2
+threads=$3
 
 sed -i '/^enabled/s/1/0/' /etc/yum/pluginconf.d/versionlock.conf
-yum --showduplicates list available --disablerepo="*" --enablerepo=${channel} | tail -n +4 > /tmp/${channel}.raw-list
+rm -f /tmp/${channel}.* 
+yum --showduplicates list available --disablerepo="*" --enablerepo=${channel} | tail -n +3 > /tmp/${channel}.raw-list
 
 sed -i ':a;/'${channel}'$/!{N;s/\n//;ba}' /tmp/${channel}.raw-list
 sed -i 's/[0-9]\{1,2\}://' /tmp/${channel}.raw-list
@@ -23,7 +25,44 @@ awk  '{print NR,$2}' /tmp/${channel}.raw-list > /tmp/${channel}.version-list
 join /tmp/${channel}.name-list /tmp/${channel}.version-list | awk '{printf("%s %s-%s\n",$1,$2,$3)}' > /tmp/${channel}.rpmlist_without_arch
 join /tmp/${channel}.rpmlist_without_arch /tmp/${channel}.arch-list | awk '{printf("%s.%s\n",$2,$3)}' > /tmp/${channel}.rpmlist
 
-for line in `cat /tmp/${channel}.rpmlist`; do yumdownloader --destdir=${dst_dir} $line; done
+line=`cat /tmp/${channel}.rpmlist | wc -l`
+each=$(($line/$threads))
+
+i=1
+while [ $i -le $threads ]; do
+        from=$((each*($i-1)+1))
+        end=$((each*$i))
+        if [ $i != $threads ]; then
+                sed -ne ''$from','$end'p' /tmp/${channel}.rpmlist > /tmp/${channel}.rpmlist.$i
+        else
+                sed -ne ''$from',$p' /tmp/${channel}.rpmlist > /tmp/${channel}.rpmlist.$i
+        fi
+	for line in `cat /tmp/${channel}.rpmlist.$i`; do yumdownloader --destdir=${dst_dir} $line; done > /tmp/${channel}.result.$i 2>&1 &
+        i=$(($i+1))
+done
+unset i
+
+sleep 5
+/usr/bin/ps aux | grep yumdownload | grep -v grep > /dev/null 2>&1
+rt=$?
+while [ $rt -eq 0 ]; do
+	sleep 5
+	/usr/bin/ps aux | grep yumdownload | grep -v grep > /dev/null 2>&1
+	rt=$?
+done 
+
+i=1
+while [ $i -le $threads ]; do
+	source_line=`cat /tmp/${channel}.rpmlist.$i | wc -l`
+	result_line=`cat /tmp/${channel}.result.$i | wc -l`
+	if [ $source_line -eq $result_line ]; then
+		echo "part $i is ok"
+	else
+		echo "part $i need to be redownloaded"
+	fi 
+        i=$(($i+1))
+done
+unset i
 
 rm -f /tmp/${channel}.* 
 sed -i '/^enabled/s/0/1/' /etc/yum/pluginconf.d/versionlock.conf
