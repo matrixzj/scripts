7ec35992d506157cf2870b90311af4f8face198e
Matrix Zou <jzou@freewheel.tv>
2016-06-22 13:19:55 +0000

    increate mtime of checking

--- config2git.sh
+++ config2git.sh
@@ -22,12 +22,12 @@ rt=$?
 project=`git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//'`
 
 if [ $rt != 0 ]; then
-	find . -type f -mmin -60 -and -not -path "./.git/*"  > /tmp/${project}_modifiled_filelist
+	find . -type f -mmin -120 -and -not -path "./.git/*"  > /tmp/${project}_modifiled_filelist
 	for file in `cat /tmp/${project}_modifiled_filelist`; do git add $file; done
 	echo "Modified files found at "  $(date +\%Y-\%m-%d\ \%H:\%M:\%S) > /tmp/${project}_commit_comment
 	for list in `cat /tmp/${project}_modifiled_filelist`; do ls -l $list >> /tmp/${project}_commit_comment; done
 	git commit -F /tmp/${project}_commit_comment
-	git push || die $0 git commit failed
+	git push || die_msg $0 git commit failed
 else
 	exit 0 
 fi
