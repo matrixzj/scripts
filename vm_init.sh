cbcf2ea5c31d24fc1619a6a9662872f44e4a4fbe
Matrix Zou <jzou@freewheel.tv>
2016-07-24 14:58:52 +0000

    vm_init.sh: modified for a new parameter

--- vm_init.sh
+++ vm_init.sh
@@ -2,6 +2,7 @@
 set timeout 5
 set consoleid [lindex $argv 0]
 set hostname [lindex $argv 1]
+set ipaddr [lindex $argv 2]
     
 spawn virsh console --force $consoleid
 expect "Escape character" 
@@ -15,7 +16,7 @@ send "df -h\n"
 expect "#"
 send "echo $hostname > /tmp/hostname\n"
 expect "#"
-send "sed -i '/^IPADDR/s/\\(IPADDR=\\).*/\\1192.168.0.0/' /tmp/ifcfg-eth0\n"
+send "sed -i \"/^IPADDR/s/\\(IPADDR=\\).*/\\1$ipaddr/\" /tmp/ifcfg-eth0\n"
 send "exit\n"
 expect "login:"
 send "\x1d"
