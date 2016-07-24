5b92bd9bfdc3db4f6de01c5a2a81b4428f13f364
Matrix Zou <jzou@freewheel.tv>
2016-07-24 14:22:22 +0000

    add a 'expect' vm init script

---
+++ vm_init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/expect 
+set timeout 5
+set consoleid [lindex $argv 0]
+set hostname [lindex $argv 1]
+    
+spawn virsh console --force $consoleid
+expect "Escape character" 
+send "\n" ;
+expect "login:" 
+send "root\n"
+expect  "Password:" 
+send "C@st20!6fw\n"
+expect "#"
+send "df -h\n"
+expect "#"
+send "echo $hostname > /tmp/hostname\n"
+expect "#"
+send "sed -i '/^IPADDR/s/\\(IPADDR=\\).*/\\1192.168.0.0/' /tmp/ifcfg-eth0\n"
+send "exit\n"
+expect "login:"
+send "\x1d"
