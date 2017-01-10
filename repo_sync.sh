9248e463ba8331de5ff93b6e4260d683bddff55a
Matrix Zou <jzou@freewheel.tv>
2017-01-10 12:45:04 +0000

    There are 2 files have been modified at 2017-01-10 12:45:04. 	-rwxr-xr-x 1 jzou ops 2748 Aug  6 06:38 config2git.sh 	-rwxr-xr-x 1 jzou ops 4515 Jan 10 12:44 repo_sync.sh There are 3 files have been added at 2017-01-10 12:45:04. 	-rwxr-xr-x 1 jzou ops 220 Jun 16  2016 bwinfo.sh 	-rw-r--r-- 1 jzou ops 281 Sep 30 01:24 changeilo.sh 	-rw-r--r-- 1 jzou ops 951 Sep 30 01:47 changeilo.txt No files have been deleted. No file type has been changed.

--- repo_sync.sh
+++ repo_sync.sh
@@ -1,95 +1,159 @@
 #! /bin/bash
 # From Matrix Zou for Repo Sync
 # Maintainer    : jzou@freewheel.tv
-# Version       : "0.2-20160418"
+# Version       : "0.3-20170110"
 #
-# $1 = channel name
-# $2 = where all rpm packages will be downloaded in
-# $3 = threads
-#
-#  04/18/2016 Matrix   enable multithreads
+#  04/18/2016 Matrix	enable multithreads
+#  01/10/2017 Matrix 	function and download retry
 
-channel=$1
-dst_dir=$2
-threads=$3
+_temp=$(mktemp -d)
+mkdir -p ${_temp}
 
-if [ "$3" == "" ] ; then
-        echo " "
-        echo "missing arguments:"
-        echo "------------------"
-        echo " arg1 = channel name (it should be exactly the same with result of 'yum repolist')"
-        echo " arg2 = path in which all rpm packages will be downloaded in "
-        echo " arg3 = threads "
-        echo " NOTE: If result path is existed, all data in it will be removed."
-        echo "------------------"
-        echo " "
+function usage() {
+        cat <<-END >&2
+	------------------
+	USAGE: repo_sync {-c channel} {-d destination_directory} {-t threads}
+	                  -c channel name  		# must be exactly the same with result of 'yum repolist')
+	                  -d destination directory	# path in which all rpm packages will be downloaded in
+	                  -t threads			# threads
+	------------------
+END
         exit
-fi
+}
 
-# disable yum plugin versionlock
-sed -i '/^enabled/s/1/0/' /etc/yum/pluginconf.d/versionlock.conf
-rm -f /tmp/${channel}.* 
+function generate_rpmlist() {
+	# disable yum plugin versionlock
+	sudo sed -i '/^enabled/s/1/0/' /etc/yum/pluginconf.d/versionlock.conf 
 
-# generate rpm list need to be downloaded
-yum --showduplicates list available --disablerepo="*" --enablerepo=${channel} | tail -n +3 > /tmp/${channel}.raw-list
-sed -i ':a;/'${channel}'$/!{N;s/\n//;ba}' /tmp/${channel}.raw-list
-sed -i 's/[0-9]\{1,2\}://' /tmp/${channel}.raw-list
-awk '{print$1}' /tmp/${channel}.raw-list | awk -F. 'BEGIN{OFS="."}{NF-=1;print}'| awk '{print NR,$0}' > /tmp/${channel}.name-list
-awk  '{print NR,$1}' /tmp/${channel}.raw-list | awk -F. '{print NR,$NF}' > /tmp/${channel}.arch-list
-awk  '{print NR,$2}' /tmp/${channel}.raw-list > /tmp/${channel}.version-list
-join /tmp/${channel}.name-list /tmp/${channel}.version-list | awk '{printf("%s %s-%s\n",$1,$2,$3)}' > /tmp/${channel}.rpmlist_without_arch
-join /tmp/${channel}.rpmlist_without_arch /tmp/${channel}.arch-list | awk '{printf("%s.%s\n",$2,$3)}' > /tmp/${channel}.rpmlist
+	# generate rpm list need to be downloaded
+	yum --showduplicates list available --disablerepo="*" --enablerepo=${channel} | tail -n +3 > ${_temp}/${channel}.raw-list
+	sed -i ':a;/'${channel}'$/!{N;s/\n//;ba}' ${_temp}/${channel}.raw-list
+	sed -i 's/[0-9]\{1,2\}://' ${_temp}/${channel}.raw-list
+	awk '{print$1}' ${_temp}/${channel}.raw-list | awk -F. 'BEGIN{OFS="."}{NF-=1;print}'| awk '{print NR,$0}' > ${_temp}/${channel}.name-list
+	awk  '{print NR,$1}' ${_temp}/${channel}.raw-list | awk -F. '{print NR,$NF}' > ${_temp}/${channel}.arch-list
+	awk  '{print NR,$2}' ${_temp}/${channel}.raw-list > ${_temp}/${channel}.version-list
+	join ${_temp}/${channel}.name-list ${_temp}/${channel}.version-list | awk '{printf("%s %s-%s\n",$1,$2,$3)}' > ${_temp}/${channel}.rpmlist_without_arch
+	join ${_temp}/${channel}.rpmlist_without_arch ${_temp}/${channel}.arch-list | awk '{printf("%s.%s\n",$2,$3)}' > ${_temp}/${channel}.rpmlist
+}
 
-# split rpmlist into $threads pieces
-total_line=`cat /tmp/${channel}.rpmlist | wc -l`
-round=$(($total_line/$threads))
-i=0
-while [ $i -le $round ]; do
-        j=1
-        while [ $j -le $threads ]; do
-                line=$(($i*$threads+$j))
-#               sed -n '/'$line'/p' /tmp/bak.${channel}.rpmlist
-                awk -v l=$line 'NR==l{print$0}' /tmp/${channel}.rpmlist >> /tmp/${channel}.rpmlist.$j
-                j=$(($j+1))
-                if [ $line -ge $total_line ]; then
-			break
-                fi
-        done
-        i=$(($i+1))
-done
-unset i 
-unset j
-unset line
+function split() {
+	parts=$1
+	# split rpmlist into ${parts}
+	total_line=$(cat ${_temp}/${channel}.rpmlist | wc -l)
+	round=$((${total_line}/${parts}))
+	i=0
+	while [ $i -le ${round} ]; do
+	        j=1
+	        while [ $j -le ${parts} ]; do
+	                line=$(($i*${parts}+$j))
+	                awk -v l=$line 'NR==l{print$0}' ${_temp}/${channel}.rpmlist >> ${_temp}/${channel}.rpmlist.$j
+	                j=$(($j+1))
+	                if [ ${line} -ge ${total_line} ]; then
+				break
+	                fi
+	        done
+	        i=$(($i+1))
+	done
+	unset i 
+	unset j
+	unset line
+	unset parts
+}
 
-# download rpm
-for i in `seq 1 $threads`; do 
-	for line in `cat /tmp/${channel}.rpmlist.$i`; do yumdownloader --destdir=${dst_dir} $line; done > /tmp/${channel}.result.$i 2>&1 &
-done
-unset i
+function download() {
+	part=$1
+	echo ${BASHPID} > ${_temp}/${channel}.rpmlist.${part}.pid
+	for line in $(cat ${_temp}/${channel}.rpmlist.${part}); do yumdownloader --destdir=${dst_dir} ${line}; done > ${_temp}/${channel}.result.${part}
+	unset line
+	unset part
+}
 
-# waiting for downloader to be finished
-sleep 5
-/usr/bin/ps aux | grep yumdownload | grep -v grep > /dev/null 2>&1
-rt=$?
-while [ $rt -eq 0 ]; do
-	sleep 5
-	/usr/bin/ps aux | grep yumdownload | grep -v grep > /dev/null 2>&1
-	rt=$?
-done 
+function check_download() {
+	local part=$1
+	local pid=$(cat ${_temp}/${channel}.rpmlist.${part}.pid)
+	/bin/ps aux | grep ${pid} | grep -v grep > /dev/null 2>&1
+	local rt=$?
+	if [ ${rt} -eq 0 ]; then
+		echo 1 > ${_temp}/part${part}.download_result
+	else 
+		echo 0 > ${_temp}/part${part}.download_result
+	fi
+	unset rt
+	unset pid
+}
 
-
-i=1
-while [ $i -le $threads ]; do
-	source_line=`cat /tmp/${channel}.rpmlist.$i | wc -l`
-	result_line=`cat /tmp/${channel}.result.$i | wc -l`
+function check_result() {
+	local part=$1
+	local source_line=$(cat ${_temp}/${channel}.rpmlist.${part} | wc -l)
+	local result_line=$(cat ${_temp}/${channel}.result.${part} | wc -l)
 	if [ $source_line -eq $result_line ]; then
-		echo "part $i is ok"
+		echo 0 > ${_temp}/part${i}.check_result
 	else
-		echo "part $i need to be redownloaded"
+		echo 1 > ${_temp}/part${i}.check_result
 	fi 
-        i=$(($i+1))
+	unset part
+}
+
+# rm -f ${_temp}/${channel}.* 
+# sed -i '/^enabled/s/0/1/' /etc/yum/pluginconf.d/versionlock.conf
+
+function init() {
+	if [ $# -lt 6 ]; then
+		usage
+	fi
+	while getopts "c:d:t:h" arg
+	do
+	        case ${arg} in
+	                c)
+				channel=${OPTARG}
+	                        ;;
+	                d)
+				dst_dir=${OPTARG}
+	                        ;;
+	                t)
+				threads=${OPTARG}
+	                        ;;
+	                h|?)
+				usage
+	                        exit 1
+	                        ;;
+	        esac
+	done
+}
+
+init $@
+generate_rpmlist
+split ${threads}
+
+for i in `seq 1 ${threads}`; do 
+	echo ${i} >> ${_temp}/download_list 
 done
 unset i
 
-# rm -f /tmp/${channel}.* 
-sed -i '/^enabled/s/0/1/' /etc/yum/pluginconf.d/versionlock.conf
+failed=1
+while [ ${failed} -gt 0 ]; do 
+	for i in `cat ${_temp}/download_list`; do 
+		download ${i} &
+	done
+	unset i
+
+	finished=1
+	while [ ${finished} -gt 0 ]; do
+		for i in `seq 1 ${threads}`; do
+		        check_download ${i}
+		done
+		unset i
+		finished=$(cat ${_temp}/part*.download_result | grep 1 | wc -l)
+	done
+	unset finished
+
+	for i in `seq 1 ${threads}`; do
+        	check_result ${i}
+	done
+	unset i
+
+	failed=$(grep 1 ${_temp}/part*.check_result | wc -l)
+	grep 1 ${_temp}/part*.check_result | awk -F':' '{print $1}' | sed -e 's/.*part\(.*\).check_result/\1/'> ${_temp}/download_list
+done
+
+rm -rf ${_temp}
